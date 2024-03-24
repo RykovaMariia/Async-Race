6ec9b8f9c27ecde077290c7156b28a1a9f56572f
Mariia Rykova <maryrykova24@gmail.com>
2024-03-25 00:11:34 +0400

    feat: add header

--- async-race/src/app/app.ts
+++ async-race/src/app/app.ts
@@ -1,8 +1,13 @@
+import { Header } from './components/header/header';
 import { router } from './router/router';
 
 export class App {
   constructor(private container: HTMLElement) {
     this.container = container;
+
+    const header = new Header(router);
+
+    this.container.append(header.getElement());
   }
 
   start() {
--- async-race/src/app/components/header/header.ts
+++ async-race/src/app/components/header/header.ts
@@ -1,7 +1,19 @@
+import { AppRoute } from '../../enums/app-route';
+import { Router } from '../../router/router';
 import { BaseComponent } from '../base-component';
+import { Button } from '../button/button';
 
 export class Header extends BaseComponent {
-  constructor() {
+  constructor(router: Router) {
     super({ tagName: 'header', classNames: 'header' });
+
+    const garageButton = new Button({ classNames: 'button', textContent: 'GARAGE' }, () =>
+      router.navigate(AppRoute.Garage),
+    );
+    const winnersButton = new Button({ classNames: 'button', textContent: 'WINNERS' }, () =>
+      router.navigate(AppRoute.Winners),
+    );
+
+    this.insertChildren([garageButton, winnersButton]);
   }
 }
